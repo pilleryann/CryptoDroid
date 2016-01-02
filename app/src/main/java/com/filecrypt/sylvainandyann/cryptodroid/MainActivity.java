1103c9d809b5b34db74c6f2cb3834e20b35ddde1
Yann Piller <yann.piller@hefr.ch>
2016-01-02 16:22:32 +0100

    - Change view text of the main view - In class activit get all view element

--- app/src/main/java/com/filecrypt/sylvainandyann/cryptodroid/MainActivity.java
+++ app/src/main/java/com/filecrypt/sylvainandyann/cryptodroid/MainActivity.java
@@ -4,13 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText password;
+    private Button enterButton;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userName = (EditText)findViewById(R.id.editTextUserName);
+        password = (EditText)findViewById(R.id.editTextPassword);
+        enterButton = (Button)findViewById(R.id.buttonEnter);
+        saveButton = (Button)findViewById(R.id.buttonSaveUser);
     }
 
 
